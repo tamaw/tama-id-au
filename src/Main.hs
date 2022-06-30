d7f86458641d4d98b75d984b81fd7896b5625847
Tama Waddell <me@tama.id.au>
2022-06-30 14:20:10 +1000

    small page to start

--- src/Main.hs
+++ src/Main.hs
@@ -6,4 +6,21 @@ main :: IO ()
 main = renderToFile "dist/index.html" masterHtml
 
 masterHtml :: Html ()
-masterHtml = p_ "hello world"
+masterHtml =
+  doctypehtml_ $ do
+    head_ headHtml
+    body_ $
+      main_ landingHtml
+
+headHtml :: Html ()
+headHtml = do
+    title_ "Blog lol"
+    meta_ [charset_ "UTF-8"]
+    meta_ [name_ "viewport", content_ "width=device-width, initial-scale=1"]
+    meta_ [name_ "description", content_ "replace me" ]
+    -- dev only
+    meta_ [httpEquiv_ "refresh", content_ "1"]
+
+landingHtml :: Html ()
+landingHtml = do
+  p_ "land here"
