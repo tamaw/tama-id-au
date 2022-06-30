2f526d771e4bef6c94ddfd0bd03684ca0a4e164d
Tama Waddell <me@tama.id.au>
2022-06-30 11:56:16 +1000

    watch support on file changes

--- src/Main.hs
+++ src/Main.hs
@@ -3,9 +3,7 @@ module Main where
 import Lucid
 
 main :: IO ()
-main = renderToFile "index.html" masterHtml
+main = renderToFile "dist/index.html" masterHtml
 
 masterHtml :: Html ()
 masterHtml = p_ "hello world"
-
-
